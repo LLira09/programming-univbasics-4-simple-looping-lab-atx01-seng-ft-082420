8ab00d5910de88df9c670062b565a366620f08f2
Jake <llira09@gmail.com>
2020-07-21 21:12:25 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,3 +22,15 @@ def output_array(array)
     counter += 1
   end 
 end 
+
+def return_string_array(array)
+  count = 0 
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s)
+    count += 1
+  end 
+  
+  
+end 
+end 
